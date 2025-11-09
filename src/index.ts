1cb0236da4c9640cc1bfc7f3db2367377ef7128e
Layssa Oliveira <layssaoliveira626@gmail.com>
2025-11-09 07:06:15 -0300

    adicionando estruturação inicial de projeto node e o index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Inicializar o projeto Node e Express
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors())
+app.use(express.json())
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+})
